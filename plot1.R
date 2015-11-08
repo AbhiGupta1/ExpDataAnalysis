0679862c964ea4154396f65d28844706aaadafe6
Abhi Gupta <abhigupta73@yahoo.com>
2015-11-09 00:08:39 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,10 @@
+source('~/load_data.R')
+
+plot1 <- paste(getwd(), "/grafics/plot1.png", sep = "")
+if(!file.exists(plot1)){
+  png("grafics/plot1.png", width = 480, height = 480)
+  hist(cargardates$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+  dev.off()
+} else {
+  hist(cargardates$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
+}
