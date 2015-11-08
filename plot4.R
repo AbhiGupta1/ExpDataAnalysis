4ec6c08b1f2949e556021877b648ae280ff01ac9
Abhi Gupta <abhigupta73@yahoo.com>
2015-11-09 00:10:39 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,23 @@
+source('~/load_data.R')
+plot4 <- paste(getwd(), "/grafics/plot4.png", sep = "")
+if(!file.exists(plot4)){
+  png("grafics/plot4.png", width = 480, height = 480)
+  par(mfrow=c(2,2))	
+  plot(cargardates$Time, cargardates$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+  plot(cargardates$Time, cargardates$Voltage, type = "l", xlab = "datetime", ylab = "Global Active Power")
+  plot(cargardates$Time, cargardates$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
+  lines(cargardates$Time, cargardates$Sub_metering_2, type="l", col="red")
+  lines(cargardates$Time, cargardates$Sub_metering_3, type="l", col="blue")
+  legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=2, col=c("black", "red", "blue"))
+  plot(cargardates$Time, cargardates$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Active Power")
+  dev.off()
+} else {
+  par(mfrow=c(2,2))	
+  plot(cargardates$Time, cargardates$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
+  plot(cargardates$Time, cargardates$Voltage, type = "l", xlab = "datetime", ylab = "Global Active Power")
+  plot(cargardates$Time, cargardates$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
+  lines(cargardates$Time, cargardates$Sub_metering_2, type="l", col="red")
+  lines(cargardates$Time, cargardates$Sub_metering_3, type="l", col="blue")
+  legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=2, col=c("black", "red", "blue"))
+  plot(cargardates$Time, cargardates$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Active Power")
+}
